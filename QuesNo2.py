9f7095908627a6922f98dbfa5ae1cbe752e0ad51
mfarhanshakil <56581125+mfarhanshakil@users.noreply.github.com>
2019-11-05 18:57:36 +0500

    Add files via upload

---
+++ QuesNo2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 16:40:42 2019
+
+@author: Farhan Shakil
+Course: Python Programming Language 
+Roll # PY01052
+Email: mfarhanshakil@gmail.com
+Assignment # 1
+Question # 2
+Python program to get the PYTHON VERSION
+"""
+
+import sys                          # Import System Modulw
+print("Python version")             # Display Python Version on Screen
+print (sys.version)                 # Read & Display Python Version 
+print("Version info.")              # Display Version Information on Screen
+print (sys.version_info)            # Read & Display Version Info 
---
+++ QuesNo6.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 17:43:11 2019
+
+@author: Farhan Shakil
+Course: Python Programming Language 
+Roll # PY01052
+Email: mfarhanshakil@gmail.com
+Assignment # 1
+Question # 6
+Python Program which Takes Two Inputs from User and Print Them Addition
+"""
+
+first_input  = input("Enter First Input  ===> ")    # Read First Input
+second_input = input("Enter Second Input ===> ")    # Read Second Input
+                                                    # Diplay Addition on Screen
+print("\n\tAddition of " + first_input + " & " + second_input + " = " + first_input + second_input)
